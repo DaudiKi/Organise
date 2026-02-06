5db5d9b600d29afb1401bdf4a2013c468cefc3a9
Uri-X <philbertkuria@gmail.com>
2026-02-07 00:56:38 +0300

    added colours for annoucement screen

--- lib/constants/colours.dart
+++ lib/constants/colours.dart
@@ -1,4 +1,11 @@
-const Color kPrimaryDark = Color(0xFF0D1B2A);
-const Color kSurfaceDark = Color(0xFF1B263B);
-const Color kAccentGold = Color(0xFFF5A623);
+import 'package:flutter/material.dart';
+
+const Color kPrimaryDark = Color(0xFF0D1B2A);      
+const Color kSurfaceDark = Color(0xFF1B263B);      
+const Color kAccentGold = Color(0xFFF5A623);      
 const Color kTextPrimary = Colors.white;
+const Color kTextSecondary = Color(0xCCFFFFFF);    
+const Color kTextTertiary = Color(0x99FFFFFF);     
+const Color kReminderColor = Color(0xFFF5A623);    
+const Color kEventColor = Colors.blueAccent;
+const Color kUpdateColor = Colors.greenAccent;
